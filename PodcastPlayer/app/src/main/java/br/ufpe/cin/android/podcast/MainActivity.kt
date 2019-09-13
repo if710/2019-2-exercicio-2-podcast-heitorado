a86f74599550588adcc0265fd49f008f87443ee0
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2019-09-13 01:19:52 -0300

    Adiciona persistência de dados com room e modifica valor passado ao adapter para utilizar dados do banco

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 import java.net.URL
 import java.util.ArrayList
 
@@ -27,35 +28,41 @@ class MainActivity : AppCompatActivity() {
 
     internal inner class downloadRssFeed : AsyncTask<Int, Int, List<ItemFeed> >() {
         override fun doInBackground(vararg resId: Int?): List<ItemFeed> {
-            var ret: List<ItemFeed> = ArrayList<ItemFeed>()
+            var parsedFeed: List<ItemFeed> = ArrayList<ItemFeed>()
 
             if(resId != null) {
                 var uri = resId[0]
                 if(uri != null){
-                    var feed = URL(getString(uri)).readText()
 
-                    ret = parseRssFeed(feed)
+                    var feed  = try {
+                        URL(getString(uri)).readText()
+                    } catch(e : Exception) {
+                        null
+                    }
+
+                    parsedFeed = parseRssFeed(feed)
+
                 }
 
             }
 
-            for(item in ret){
-                Log.d("RET", item.toString())
+            val db = ItemFeedDB.getDatabase(applicationContext)
+
+            for(item in parsedFeed){
+                //Log.d("RET", item.toString())
+                db.itemFeedDAO().insertItems(item)
             }
 
-            return ret
+            // Retorna SELECT * do db ao invés do parsedFeed.
+            return db.itemFeedDAO().allItems()
         }
 
         override fun onPostExecute(result: List<ItemFeed>?) {
-            var itemList: List<ItemFeed>
-
             if(result != null) {
-                itemList = result
+                listRecyclerView.adapter = ItemFeedAdapter(result, applicationContext)
             } else {
-                itemList = ArrayList<ItemFeed>()
+                listRecyclerView.adapter = ItemFeedAdapter(ArrayList<ItemFeed>(), applicationContext)
             }
-
-            listRecyclerView.adapter = ItemFeedAdapter(itemList, applicationContext)
         }
 
         private fun parseRssFeed(feed: String?): List<ItemFeed>{
